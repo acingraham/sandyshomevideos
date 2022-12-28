ba404fe18ec10d83e8b133b5c7d68a9fe7bee1c5
Andrew Ingraham <acingraham@gmail.com>
2022-12-28 11:18:37 -0600

    Log initial page load and url changes to amplitude

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 import * as amplitude from '@amplitude/analytics-browser'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 if (process.env.NEXT_PUBLIC_AMP_KEY) {
   amplitude.init(process.env.NEXT_PUBLIC_AMP_KEY)
@@ -22,6 +24,23 @@ amplitude.init(`${process.env.NEXT_PUBLIC_AMP_KEY}`, undefined, {
 */
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  // Captures every url change including initial page load
+  useEffect(() => {
+    amplitude.track(`Viewed ${router.asPath}`)
+
+    const handleRouteChange = (url: string) => {
+      amplitude.track(`Viewed ${url}`)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
   return (
     <>
       <Component {...pageProps} />
